d031c9a4ada7441c659adf21200943ce611be7a0
rumeshnw <rumesh18@gmail.com>
2024-06-09 22:16:35 +1000

    Added log config

--- src/main/java/dev/rumesh/homelibrary/web/controller/BookController.java
+++ src/main/java/dev/rumesh/homelibrary/web/controller/BookController.java
@@ -1,17 +1,15 @@
 package dev.rumesh.homelibrary.web.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-
 import dev.rumesh.homelibrary.domain.Book;
 import dev.rumesh.homelibrary.service.BookService;
 import dev.rumesh.homelibrary.web.dto.BookDto;
 import dev.rumesh.homelibrary.web.mapper.BookMapper;
 import lombok.extern.slf4j.Slf4j;
-
-import java.util.List;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
@@ -26,9 +24,9 @@ public class BookController {
         this.bookService = bookService;
         this.bookMapper = bookMapper;
     }
-    
+
     @GetMapping("")
-    public List<BookDto> getAllBooks() {
+    public List<BookDto> getAllBooks(){
         log.info("Loading all books");
         List<Book> books = bookService.getAllBooks();
         return books.stream().map(bookMapper::toDto).toList();
